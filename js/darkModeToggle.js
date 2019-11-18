545d3da0a43e4cbf8bb53b7180767c9390faba9f
Cole Goodnight <goodnic@CF165-24.cs.wwu.edu>
2019-11-18 11:54:10 -0800

    working on dark mode script

---
+++ js/darkModeToggle.js
@@ -0,0 +1,4 @@
+function darkModeToggle() {
+    // toggle dark stuff
+    document.getElementById("header").classList.toggle('darkHeader');
+}
--- js/weatherScript.js
+++ js/weatherScript.js
@@ -1,5 +1,6 @@
 // don't know js but it's basically the same as java amrite?
-function displayWeather() {
+// 
+function definitelyARealWeatherAPI() {
     var weatherStates = ["Sunny", "Cloudy", "Clear", "Monsoon", "Light Rain", "Literal Volcano Eruption", "Cloudy with a Chance of Meatballs"]
     document.getElementById("temp").innerHTML = Math.round(Math.random()*60 + 32);
     document.getElementById("humid").innerHTML = Math.round(Math.random()*100) + "%";
