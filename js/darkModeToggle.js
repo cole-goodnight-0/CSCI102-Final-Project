feb9566efcc40e50636a8b9651d8ba32f6f95896
Knoticalcosmo <jonwstoneman@gmail.com>
2019-11-21 08:41:49 -0800

    Gave each navigation element an id so that dark mode can affect these

--- js/darkModeToggle.js
+++ js/darkModeToggle.js
@@ -2,7 +2,12 @@ function darkModeToggle() {
     // toggle dark stuff
     document.getElementById('notheader').classList.toggle('darkHeader');
     document.getElementById('notnav').classList.toggle('darkNav');
-    document.getElementsByClassName('notnava').classList.toggle('darkNavA');
+    //document.getElementsByClassName('notnava').classList.toggle('darkNavA'); //Cannot read property 'toggle', thus just made a bunch of ids
+    document.getElementById('navHome').classList.toggle('darkNavA');
+    document.getElementById('navCN').classList.toggle('darkNavA');
+    document.getElementById('navWN').classList.toggle('darkNavA');
+    document.getElementById('navSN').classList.toggle('darkNavA');
+    document.getElementById('navWeather').classList.toggle('darkNavA');
     document.getElementById('notmain').classList.toggle('darkMain');
     document.getElementById('notfooter').classList.toggle('darkFooter');
 
